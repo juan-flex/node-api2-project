5e364c5a63087dc7fefd1070611d552d59bec91a
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-07-09 15:47:48 -0400

    Stretch

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express'); // importing express
+const cors = require('cors');
 const postsRouter = require('./posts/posts-router')// importing posts-router
 
 const server = express(); //creating express server
 server.use(express.json()); // making server to use express.json middleware
-
+server.use(cors());
 // router
 server.use('/api/posts', postsRouter ) // using posts-router endpoints
 
